d2dbff723d1ded5c6b6810d524c5728dcc68a189
KarlYu130 <klkyu@connect.ust.hk>
2023-10-25 14:47:34 +0800

    comment updated

--- RM2024-RDC-Core/Drivers/PID.cpp
+++ RM2024-RDC-Core/Drivers/PID.cpp
@@ -11,8 +11,8 @@ float PID::update(float target, float measurement, float dt)
     /*=====================================================================*/
     error = target - measurement;  // Calculate the error
     pOut = Kp * error;             // Calculate the P term
-    iOut += Ki * error * dt;   
-    dOut = Kd * (error-lastError)/dt;   // Calculate the I term
+    iOut += Ki * error * dt;    // Calculate the I term
+    dOut = Kd * (error-lastError)/dt;   // Calculate the D term
     output = pOut + iOut + dOut ;  // The output is the sum of all terms
     lastError = error;     // Update the last error
     /*=====================================================================*/
