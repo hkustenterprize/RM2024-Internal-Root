bfa6689e81cb449a19032d7b943b36dd3b611132
Philoism <Philoismism@gmail.com>
2023-10-26 23:33:30 +0800

    Some comments

--- RM2024-RDC-Core/Drivers/DJIMotor.cpp
+++ RM2024-RDC-Core/Drivers/DJIMotor.cpp
@@ -8,54 +8,9 @@
 
 namespace DJIMotor
 {
-<<<<<<< HEAD
 // Initialize motor's controller instance
 DJIMotor motors[8];
 
-
-
-int16_t DJIMotor::getRPM()
-{
-    // uint32_t mailbox;
-    // HAL_CAN_GetRxMessage(&hcan, CAN_RX_FIFO0, &rxheader, rxData);
-
-    // rpm = rxData[2]<<8 | rxData[3];
-
-    // HAL_Delay(1);
-    // return 0.0f;
-=======
-
-uint8_t txData[8]            = {};
-uint8_t rxData[8]            = {};
-CAN_TxHeaderTypeDef txHeader = {0x1FF, 0, CAN_ID_STD, CAN_RTR_DATA, 8, DISABLE};
-CAN_FilterTypeDef filter     = {0,
-                                0x205 << 5,
-                                0,
-                                0,
-                                CAN_FILTER_FIFO0,
-                                ENABLE,
-                                CAN_FILTERMODE_IDMASK,
-                                CAN_FILTERSCALE_16BIT,
-                                CAN_FILTER_ENABLE,
-                                0};
-CAN_RxHeaderTypeDef rxheader;
-//int16_t rpm;
-
-// Initialize motor's controller instance
-DJIMotor motors[8];
-
-/*========================================================*/
-// Your implementation of the function, or even your customized function, should
-// be implemented here
-/*========================================================*/
-/**
- * @todo
- */
-void init() {
-    HAL_CAN_ConfigFilter(&hcan, &filter);
-  HAL_CAN_Start(&hcan);
->>>>>>> ad723e03a2a42cba7373e10ee096fdd6243668a4
-}
 
 /**
  * @brief Get the raw encoder value
